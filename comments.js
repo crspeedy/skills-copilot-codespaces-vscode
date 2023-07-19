9ce791b4f040d6871c55a14f588d7b7450cf5d5c
crspeedy <115433656+crspeedy@users.noreply.github.com>
2023-07-19 04:58:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,85 @@
 //create web server
-const express = require('express');
-const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
-app.use(express.json({limit: '1mb'}));
+const express = require("express");
+const router = express.Router();
+//import model for comments
+const Comment = require("../models/comment");
+//import middleware
+const { jwtAuth } = require("../middleware/auth");
+//import error handler
+const { errorHandler } = require("../middleware/error");
+//import mongoose
+const mongoose = require("mongoose");
+//import model for users
+const User = require("../models/user");
 
-//create database
-const Datastore = require('nedb');
-const database = new Datastore('database.db');
-database.loadDatabase();
-
-//save data into database
-app.post('/api', (request, response) => {
-    console.log('I got a request!');
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json(data);
+//get all comments
+router.get("/", jwtAuth, async (req, res, next) => {
+  try {
+    //get all comments
+    const comments = await Comment.find();
+    //return comments
+    return res.json(comments);
+  } catch (err) {
+    return next(err);
+  }
 });
 
-//get data from database
-app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
-        if(err){
-            response.end();
-            return;
-        }
-        response.json(data);
-    });
+//get all comments for a post
+router.get("/:postId", jwtAuth, async (req, res, next) => {
+  try {
+    //get all comments for a post
+    const comments = await Comment.find({ postId: req.params.postId });
+    //return comments
+    return res.json(comments);
+  } catch (err) {
+    return next(err);
+  }
 });
+
+//add a comment
+router.post("/", jwtAuth, async (req, res, next) => {
+  try {
+    //create comment
+    const comment = await Comment.create(req.body);
+    //return comment
+    return res.json(comment);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//update a comment
+router.patch("/:id", jwtAuth, async (req, res, next) => {
+  try {
+    //find comment
+    const comment = await Comment.findById(req.params.id);
+    //if comment not found
+    if (!comment) {
+      //throw error
+      throw new Error("Comment not found");
+    }
+    //update comment
+    comment.set(req.body);
+    //save comment
+    await comment.save();
+    //return updated comment
+    return res.json(comment);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+//delete a comment
+router.delete("/:id", jwtAuth, async (req, res, next) => {
+  try {
+    //find comment
+    const comment = await Comment.findById(req.params.id);
+    //if comment not found
+    if (!comment) {
+      //throw error
+      throw new Error("Comment not found");
+    }
+    //delete comment
+    await comment.remove();
+    //return message
+    return
